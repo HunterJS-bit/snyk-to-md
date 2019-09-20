b40b39c356779302ab0f8bbb63ebc29991ae2ee2
Marko <markomiljkovic2@gmail.com>
2019-09-21 00:11:47 +0200

    added readline, and reading from stdin

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
-export * from './lib/async';
-export * from './lib/hash';
-export * from './lib/number';
+const minimist = require('minimist');
+var readline = require('readline');
+
+const argv = minimist(process.argv.slice(2));
+
+let jsonString = '';
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+
+async function readData() {
+    return new Promise((resolve, reject) => {
+
+        rl.on('line', (line: String) => {
+            jsonString += line;
+        }).on('close', () => {
+            resolve('finished');
+        }).on('error', (err: Object) => {
+            reject(err);
+        })
+
+    });
+}
+
+(async function () {
+    try {
+        await readData();
+        const data = JSON.parse(jsonString);
+        console.log('Data read from input ');
+        console.log(data.summary);
+
+    } catch (e) {
+        console.error(e);
+    }
+})()
