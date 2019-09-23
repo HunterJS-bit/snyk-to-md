8ec57de695768704464feecaf01ac6b30734df56
Marko <markomiljkovic2@gmail.com>
2019-09-23 23:17:39 +0200

    adding formating to snyk output

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 const minimist = require('minimist');
-var readline = require('readline');
+const readline = require('readline');
+const formatingUtil = require('./lib/formatingUtil');
+const fs = require('fs');
 
 const argv = minimist(process.argv.slice(2));
+
 
 let jsonString = '';
 
@@ -38,11 +41,11 @@ async function readData() {
             console.log(data[0].severity);
             */
             var sortOrder = ['h', 'm', 'l'];
-            const sortedVulnerabilities = data.vulnerabilities.sort(function (a, b) {
+            const sortedVulnerabilities = data.vulnerabilities.sort(function (a: any, b: any) {
                 return sortOrder.indexOf(a.severity) - sortOrder.indexOf(b.severity);
             });
 
-            const mappedList = sortedVulnerabilities.map(e => {
+            const mappedList = sortedVulnerabilities.map((e: any) => {
                 return {
                     title: e.title,
                     packageName: e.name,
@@ -50,12 +53,27 @@ async function readData() {
                     priority: e.severity,
                     packageManager: e.packageManager,
                     from: e.from.join(' > '),
-                    description: e.description
+                    description: e.description,
                 }
             })
 
-            console.log('Mappeddd ');
-            console.log(mappedList);
+            let content = `# Snyk Vulnerability Report \n`;
+            mappedList.forEach(el => {
+                content += `### ✗ ${el.title} 
+                - Introduced through: ${el.packageName}
+                - From: ${el.from} 
+                - Package Manager: ${el.packageManager}`;
+
+            });
+
+
+            fs.writeFile('./test.md', content, (err: any) => {
+                if (err) {
+                    console.error(err)
+                    return;
+                }
+            });
+
 
             return;
         }
